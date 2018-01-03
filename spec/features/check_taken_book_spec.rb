ab83eac203b66c621db5cf295dacdd572d4f9811
vladfreel <vladfreelmaster@gmail.com>
2018-01-03 17:24:13 +0200

    add comment, take and return book, and check taken book

---
+++ spec/features/check_taken_book_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+describe 'add like to book' do
+  let(:user) { FactoryBot.create(:user) }
+  let(:user2) { FactoryBot.create(:user, email: 'gggggg@gmail.com') }
+  it 'should add book', js: true do
+    visit new_user_session_path
+    fill_in 'email', with: user.email
+    fill_in 'pass', with: user.password
+    click_button 'Sign in'
+    click_on('Добавить книгу')
+    fill_in 'book_name', with: 'книга'
+    fill_in 'book_genre', with: 'жанр книги'
+    fill_in 'book_author', with: 'автор книги'
+    attach_file('book[img]',
+                File.join(Rails.root, '/spec/fixtures/doska12.jpg'))
+    fill_in 'book_description', with: 'описание книги'
+    click_on('Create Book')
+    expect(Book.count).to eq(1)
+    book = Book.last.id
+    expect(current_path) == book_path(id: book)
+    click_on('Взять')
+    expect(History.count).to eq(1)
+    click_on('Главная')
+    click_on('Выйти')
+    fill_in 'email', with: user2.email
+    fill_in 'pass', with: user2.password
+    click_button 'Sign in'
+    visit book_path(book)
+    expect(page).to have_no_content('Взять')
+    expect(page).to have_no_content('Вернуть')
+  end
+end
